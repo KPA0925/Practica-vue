543f3fd0c2fc034541848cb31f91ff1d417ef4c8
Kevin Piedrahita <kevin.piedrahita.11c@gmail.com>
2025-09-17 12:11:27 -0500

    Crea componente lista de tareas borra componentes autogenerados crea rutas

---
+++ src/components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sidebar">
+        <h2>barra lateral</h2>
+        <ul>
+            <li><RouterLink to="/contador">Contador</RouterLink></li>
+            <li><RouterLink to="/tareas">Tareas</RouterLink></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .sidebar {
+        width: 250px;
+        background-color: #f4f4f4;
+        padding: 20px;
+        box-shadow: 2px 0 5px rgba(0,0,0,0.1);
+    }
+</style>
